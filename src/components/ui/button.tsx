401f809c505f2da1db8649f2eabb3604c71abcd6
syedmuhammadhussain <syedmuhammadhussain6@gmail.com>
2025-07-20 14:04:31 +0500

    update button style

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,7 +3,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const buttonVariants = cva(
-  "cursor-pointer inline-flex items-center justify-center select-none font-bold transition-all duration-150 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2",
+  "cursor-pointer inline-flex items-center justify-center select-none font-medium transition-all duration-150 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2",
   {
     variants: {
       variant: {
@@ -13,20 +13,19 @@ const buttonVariants = cva(
           "text-lg rounded-full",
           "bg-blue-500 text-white border border-blue-400",
           "shadow-[0_5px_0_0_#0552d1,0_12px_4px_0_#1b70f841]",
-          // "ring-4 ring-gray-200 ring-offset-2 ring-offset-white",
           "active:translate-y-1 active:shadow-none active:border-b-0",
         ].join(" "),
         white3d: [
           "text-lg rounded-full",
           "bg-white text-black border border-white-400",
-          "shadow-[0_5px_0_0_#f8f8f8,0_12px_4px_0_#cdcdcd42]",
+          "shadow-[0_5px_0_0_#f8f8f8,0_12px_4px_0_#92929242]",
           "active:translate-y-1 active:shadow-none active:border-b-0",
         ].join(" "),
       },
       size: {
         default: "px-6 py-3",
         sm: "px-3 py-2 text-sm",
-        lg: "px-8 py-4 text-base",
+        lg: "px-5 py-3 text-base",
         icon: "p-2",
       },
     },
